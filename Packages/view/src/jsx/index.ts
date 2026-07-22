93f457c1680fd016d4ec3dd35c52f867906e28ba
Zastinian <zastinian@gmail.com>
2026-07-21 19:48:25 -0600

    refactor: Revert JSX module exports to direct re-export

--- Packages/view/src/jsx/index.ts
+++ Packages/view/src/jsx/index.ts
@@ -3,5 +3,4 @@
  */
 
 export type { JSX } from "../jsx.d";
-export { effect, Fragment, jsx, jsxs } from "./element";
-export { SVG_ATTR_MAP, SVG_ELEMENTS, SVG_NS } from "./svg-map";
+export { effect, Fragment, jsx, jsxs, SVG_ATTR_MAP, SVG_ELEMENTS, SVG_NS } from "./element";
